722bcf082127ebc5b44c2ce4a86480ef82de534e
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2018-12-28 14:56:00 +0900

    add textlint / marked / lodash

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="editor">
+    <textarea :value="input" @input="update"></textarea>
+    <div v-html="compiledMarkdown"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import {Component, Prop, Vue, Watch} from "vue-property-decorator";
+import { TextlintKernel } from '@textlint/kernel';
+import _ from 'lodash';
+import marked from 'marked';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  input: string = '# hello Vue.js';
+
+  debounce = _.debounce;
+
+  get compiledMarkdown() {
+      return marked(this.input, {sanitize: true});
+  }
+
+    update(e) {
+        console.log('debounce');
+        this.input = e.target.value;
+    }
+
+    mounted() {
+        this.update = _.debounce(this.update, 300);
+
+      const kernel = new TextlintKernel();
+
+      const options = {
+          ext: '.md',
+          plugins: [
+              {
+                  pluginId: 'markdown',
+                  plugin: require('@textlint/textlint-plugin-markdown'),
+              },
+          ],
+          // rules: require('textlint-rule-preset-ja-spacing/src'),
+          rules: [
+              {
+                  ruleId: 'no-mix-dearu-desumasu',
+          //         // rule: require('textlint-rule-no-mix-dearu-desumasu'),
+          //         // rule: require('textlint-rule-no-todo'),
+          //         rule: require('textlint-rule-preset-ja-spacing/src'),
+                  rule: require('textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana'),
+
+              },
+          ],
+      };
+
+      // console.log(options);
+
+      kernel.lintText('パソコン　ソフトはすごい。', options).then(result => {
+          console.log(result);
+      });
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+html, body, #editor {
+  margin: 0;
+  height: 100%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #333;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+
+textarea, #editor div {
   display: inline-block;
-  margin: 0 10px;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
 }
-a {
-  color: #42b983;
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
 }
 </style>
