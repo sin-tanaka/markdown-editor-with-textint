1a1783b9f513b372dba83fa99ef9f0375cf11adb
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2019-02-06 00:33:04 +0900

    add rules.d.ts

---
+++ src/@types/rules.d.ts
@@ -0,0 +1,3 @@
+declare module 'textlint-rule-no-todo';
+declare module 'textlint-rule-ja-no-space-around-parentheses';
+declare module 'textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana';
